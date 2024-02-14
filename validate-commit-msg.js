97c111b931e184e3f8b3c00f6bd278ca46110b7b
Masalski Sergey <masalskiysergey@gmail.com>
2024-02-14 22:29:03 +0300

    ASTONJS-1 [fix]: fix pattern

--- validate-commit-msg.js
+++ validate-commit-msg.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 const commitMessageFile = process.argv[2];
 const textMessage = fs.readFileSync(commitMessageFile, 'utf-8').trim();
 
-const pattern = /^ASTONJS-([1-9]|[1-9]\d) (feat|fix|chore):/;
+const pattern = /^ASTONJS-([^0]\d*) (\[feat\]|\[fix\]|\[chore\]):/;
 
 if (pattern.test(textMessage)) {
   console.log('Commit message validation passed.');
   process.exit(0);
 }
 
-console.log('Error: Your commit massege must begin with ASTONJS-[1-99] [feat/fix/chore]:');
+console.log('Error: Your commit massege must begin with ASTONJS-Number [feat]/[fix]/[chore]:');
 process.exit(1);
