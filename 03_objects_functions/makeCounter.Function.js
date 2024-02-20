0499388905396006bd728121e1be38409c921240
Masalski Sergey <masalskiysergey@gmail.com>
2024-02-20 20:09:29 +0300

    ASTONJS-3 [feat]: finished homework-3

---
+++ 03_objects_functions/makeCounter.Function.js
@@ -0,0 +1,23 @@
+// First method:
+
+// function makeCounter(counter) {
+//   return ++counter;
+// }
+
+// Second method:
+
+// const makeCounter = function (counter) {
+//   return ++counter;
+// };
+
+// Third method:
+
+// const makeCounter = counter => {
+//   return ++counter;
+// };
+
+// Fourth method:
+
+// const functionBody = 'return ++counter';
+
+// const makeCounter = new Function('counter', functionBody);
