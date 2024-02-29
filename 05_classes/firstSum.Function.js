f81ca1d3aad5700fc46d8ce31b6ab0544b5878fb
Masalski Sergey <masalskiysergey@gmail.com>
2024-02-29 16:00:51 +0300

    ASTONJS-5 [fix]: add method

--- 05_classes/firstSum.Function.js
+++ 05_classes/firstSum.Function.js
@@ -1,25 +1,45 @@
 // First method
 
-function firstSum(arr, total) {
-  let isFound = false;
-  const result = [];
+// function firstSum(arr, total) {
+//   let isFound = false;
+//   const result = [];
 
-  arr.forEach((item, index, array) => {
-    if (isFound) return;
-    array.forEach((itemIterator, indexIterator) => {
-      if (isFound) return;
-      if (indexIterator === index) return;
-      if (item + itemIterator === total) {
-        isFound = true;
-        result.push(item, itemIterator);
-      }
-    });
-  });
+//   arr.forEach((item, index, array) => {
+//     if (isFound) return;
+//     array.forEach((itemIterator, indexIterator) => {
+//       if (isFound) return;
+//       if (indexIterator === index) return;
+//       if (item + itemIterator === total) {
+//         isFound = true;
+//         result.push(item, itemIterator);
+//       }
+//     });
+//   });
 
-  if (!isFound) return 'Incorrect array or sum';
+//   if (!isFound) return 'Incorrect array or sum';
 
-  return result;
-}
+//   return result;
+// }
 
 // Данная реализация функции представляет собой квадратичную сложность О(n^2) так как на
 // каждый элемента массива она производит по n=== array.length оппераций сравнения
+
+// Second method
+
+function firstSum(arr, total) {
+  const addItemCollection = new Map();
+
+  for (let item of arr) {
+    const addItem = total - item;
+
+    if (addItemCollection.has(addItem)) return [addItem, item];
+
+    addItemCollection.set(item, item);
+  }
+
+  return 'Incorrect array or sum';
+}
+
+// Map реализован на механизме схожем с хеш-таблицами, соответственно доступ к значению будет иметь
+// сложность O(1) и мы зависим от длины массива переданных данных, то есть от n. Соответственно
+// сложность алгоритма О(n)
